a4cac8b45c01886175b1af97bc165e87bb9bbb21
EdgarMG0410 <emancillas04@gmail.com>
2022-08-03 20:30:19 -0500

    :white_check_mark: GIF Expert App Finished

--- gif-expert-app/src/components/GifGrid.jsx
+++ gif-expert-app/src/components/GifGrid.jsx
@@ -1,16 +1,27 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getGifs } from "../helpers/getGifs"
+import { useFetchGifs } from '../hooks/useFetchGifs';
+import { GifItem } from './GifItem';
 
 
 export const GifGrid = ({ category }) => {
 
-    useEffect( () =>{
-        getGifs(category);
-    }, [])
-    
+    const {images, isLoading} = useFetchGifs( {category });
+
     return (
         <>
-            <h3>{ category }</h3>
+            <h3>{category}</h3>
+            {
+                isLoading ? (
+                    <h2>Cargando...</h2>
+                ): null
+            }
+            <div className='card-grid'>
+                {images.map((image) => (
+                   <GifItem {...image}/>
+                ))
+                }
+            </div>
         </>
     )
 }
